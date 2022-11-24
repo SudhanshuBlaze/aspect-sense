9f8e1bd5d48637e2c1313fca36b79e27aa497dae
SudhanshuBlaze <skrourkela@gmail.com>
2022-11-24 21:01:00 +0530

    fix file name typo

--- reviews_scrapper.py
+++ reviews_scrapper.py
@@ -4,9 +4,20 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 import time
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def reviews():
@@ -49,3 +60,6 @@ def reviews():
         reviews_arr.append(each_review.text)
     
     return {"reviews": reviews_arr}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, debug=True)
