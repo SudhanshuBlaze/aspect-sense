68f5045a82e776448f7fbdbb8520dbe55f733df3
SudhanshuBlaze <skrourkela@gmail.com>
2022-11-28 23:23:16 +0530

    remove unncessary code from endpoint, made global

--- reviews_scrapper.py
+++ reviews_scrapper.py
@@ -24,32 +24,26 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# loads chrome driver
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+options.add_argument('-headless')
+options.add_argument('-no-sandbox')
+options.add_argument('-disable-dev-shm-usage')
+
+# url of the location on google maps, whose review needs to be scrapped
+url="https://www.google.com/maps/place/State+Bank+Of+India/@22.2558208,84.8882918,16.67z/data=!4m7!3m6!1s0x3a201c2a222c256b:0xd43e5ebc8e9da67b!8m2!3d22.2552897!4d84.8935059!9m1!1b1"
+
+# initializing the webdriver
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+
 @app.get('/')
 def reviews():
-    options = webdriver.ChromeOptions()
-    # options= Options()
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    options.add_argument('-headless')
-    options.add_argument('-no-sandbox')
-    options.add_argument('-disable-dev-shm-usage')
-
-
-    # url="https://www.google.com/maps/place/Happy+Home+School/@22.2618037,84.8870326,19.26z/data=!4m7!3m6!1s0x3a201d9167666c77:0x7a65f976c2b26d33!8m2!3d22.2622776!4d84.8867239!9m1!1b1"
-    url="https://www.google.com/maps/place/State+Bank+Of+India/@22.2558208,84.8882918,16.67z/data=!4m7!3m6!1s0x3a201c2a222c256b:0xd43e5ebc8e9da67b!8m2!3d22.2552897!4d84.8935059!9m1!1b1"
-
-    # url="https://www.google.com/maps/place/CWS+Hospital/@22.2558208,84.8882918,16z/data=!4m14!1m6!3m5!1s0x3a201c2a222c256b:0xd43e5ebc8e9da67b!2sState+Bank+Of+India!8m2!3d22.2552897!4d84.8935059!3m6!1s0x3a201c2ca6e5da79:0xd1e929836dc72b94!8m2!3d22.255401!4d84.9008933!9m1!1b1"
-
-    
-
-    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
-    # browser = webdriver.Chrome('./chromedriver',options=options)
 
     browser.get(url)
-    # print(browser.page_source)  # results
 
     num_of_reviews=browser.find_element(By.XPATH,'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]/div[2]/div/div[2]/div[2]').text.split(" ")[0]
    
-
     #Find scroll layout
     scrollable_div = browser.find_element(By.XPATH,'//*[@id="QA0Szd"]/div/div/div[1]/div[2]/div/div[1]/div/div/div[2]')
 
@@ -60,6 +54,7 @@ def reviews():
                 scrollable_div)
         time.sleep(2)
 
+    #Find all reviews
     review_span = browser.find_elements(by=By.CLASS_NAME,value="wiI7pd")
     rating_span = browser.find_elements(by=By.CLASS_NAME,value="kvMYJc")
     
