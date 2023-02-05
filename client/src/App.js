a8b44af7cd822e03d1f845e154cceecfb2aa7c5e
SudhanshuBlaze <skrourkela@gmail.com>
2023-02-05 23:41:53 +0530

    client calls the api and renders a table for json

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,65 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 function App() {
-  return <div>Hi</div>;
+  const [url, setUrl] = useState("");
+  const [data, setData] = useState([]);
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    const result = await axios.get(`http://localhost:8000/?url=${url}`);
+    setData(result.data);
+  };
+
+  console.log(data);
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={url}
+          onChange={e => setUrl(e.target.value)}
+          style={{ width: "80%", padding: "10px", marginRight: "10px" }}
+        />
+        <button
+          type="submit"
+          style={{
+            padding: "10px",
+            backgroundColor: "#4CAF50",
+            color: "white",
+          }}
+        >
+          Submit
+        </button>
+      </form>
+      {data.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              <th>Review</th>
+              <th>Ratings</th>
+              <th>Segmented Reviews</th>
+              <th>Aspect with Description</th>
+              <th>Aspect with Polarity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, index) => (
+              <tr key={index}>
+                <td>{row.reviews}</td>
+                <td>{row.ratings}</td>
+                <td>[{row.segmented_reviews.toString()}]</td>
+
+                <td>[{JSON.stringify(row.aspect_with_description)}]</td>
+                <td>[{JSON.stringify(row.aspect_with_polarity)}]</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 }
 
 export default App;
