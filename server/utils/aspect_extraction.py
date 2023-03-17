9351f033af5d7280193c6ca16151c56912599fab
SudhanshuBlaze <skrourkela@gmail.com>
2023-03-18 01:37:01 +0530

    clean up of aspect_extraction

--- server/utils/aspect_extraction.py
+++ server/utils/aspect_extraction.py
@@ -1,63 +1,53 @@
 from textblob import TextBlob
 import spacy
-nlp=spacy.load("en_core_web_sm")
+
+nlp = spacy.load("en_core_web_sm")
 
 def get_aspect(doc):
-    aspect = ''
     for token in doc:
         if token.dep_ == 'nsubj' and token.pos_ == 'NOUN':
-              aspect = token.text
-    return aspect
+            return token.text
+    return ''
 
 def get_descriptor(doc):
     descriptive_term = ''
-    for i,token in enumerate(doc):
+    for i, token in enumerate(doc):
         if token.pos_ == 'ADJ':
-            prepend = ''
-            for child in token.children:
-                if child.pos_ == 'ADV':
-                    prepend += child.text + ' ' #Ex: not very good
-            
-            negation=""
-            for t in range(i,0,-1):
-                    try:
-                        if doc[i-t].dep_ == 'neg':
-                            negation = doc[i-t].text+' '
-                    except:
-                        continue
-            descriptive_term = negation+ prepend + token.text
+            prepend = ' '.join([child.text for child in token.children if child.pos_ == 'ADV'])+ ' '
+
+            negation = ''
+            for t in range(i, 0, -1):
+                if i - t >= 0 and doc[i-t].dep_ == 'neg':
+                    negation = doc[i-t].text + ' '
+                    break
+            descriptive_term = negation + prepend + token.text
+            break
     return descriptive_term
 
 def get_polarity(segment):
-    """
-    Predict the polarity of the text using TextBlob.
-    Results range from negative to positive on a scale of [-1, +1].
-    """
-    polarity=round(TextBlob(segment).sentiment.polarity, 2)
+    polarity = round(TextBlob(segment).sentiment.polarity, 2)
     return polarity
 
-# get_polarity("architecture is not very great")
 
 def analysis_with_spacy(reviews_list):
-    aspects = []
-    polarities=[]
+    aspects ,polarities = [], []
     for segment in reviews_list:
         doc = nlp(segment)
         
         #Use TextBlob to get sentiments
-        polarity=get_polarity(segment)
+        polarity = get_polarity(segment)
 
         #extract aspect
-        aspect=get_aspect(doc)  
+        aspect = get_aspect(doc)  
         
         #extract descriptor
-        descriptive_term=get_descriptor(doc)
+        descriptive_term = get_descriptor(doc)
         
-        if aspect!="" and descriptive_term!="":
-            aspects.append({'aspect': aspect,'description': descriptive_term})
-            polarities.append({'aspect': aspect,'polarity': polarity})
+        if aspect and descriptive_term:
+            aspects.append({'aspect': aspect, 'description': descriptive_term})
+            polarities.append({'aspect': aspect, 'polarity': polarity})
         else:
-            aspects.append({'aspect': None,'description': None})
-            polarities.append({'aspect': None,'polarity': None})
+            aspects.append({'aspect': None, 'description': None})
+            polarities.append({'aspect': None, 'polarity': None})
             
-    return aspects,polarities
+    return aspects, polarities
