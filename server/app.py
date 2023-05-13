93e42b104a861140e0eeaef8a8380e24b6801d0f
SudhanshuBlaze <skrourkela@gmail.com>
2023-05-14 00:48:55 +0530

    handled error when db connection fails

--- server/app.py
+++ server/app.py
@@ -21,8 +21,6 @@ app.include_router(semantic_search, tags=["semantic_search"])
 @app.on_event("startup")
 async def startup_event():
     Database()
-    print("Connected to the database")
-
 # demo_url="https://www.google.com/maps/place/S+K+Family+restaurant/@19.3578934,84.8719974,17z/data=!4m8!3m7!1s0x3a3d575facaae5f5:0x5f3c6df83908b341!8m2!3d19.3578884!4d84.8741861!9m1!1b1!16s%2Fg%2F11nddfbs86"
 
 
--- server/database/database.py
+++ server/database/database.py
@@ -6,6 +6,10 @@ class Database:
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super().__new__(cls)
-            client = MongoClient("mongodb://localhost:27017/")
-            cls._instance.db = client["googlemaps"]
-        return cls._instance         
+            try:
+                client = MongoClient("mongodb://localhost:27017/")
+                cls._instance.db = client["googlemaps"]
+                print("\033[95;1m\033[1ASuccessfully connected to database.\033[0m")
+            except Exception as e:
+                print("\033[91;1m\033[1AError connecting to database: {} {}\033[0m".format(type(e).__name__, str(e)))
+        return cls._instance
